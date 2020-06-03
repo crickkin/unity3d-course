0148a7a06c050067d699f15699af8641d2b4fc6d
crickkin <37256927+crickkin@users.noreply.github.com>
2020-06-03 18:36:27 -0300

    aula 43

---
+++ Assets/Sections/03-Project boost/Scripts/RocketBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space Press");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
+    }
+}
