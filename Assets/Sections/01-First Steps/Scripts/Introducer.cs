84693a392bc98f5aafcc118f056137ea1f05de7e
crickkin <37256927+crickkin@users.noreply.github.com>
2020-06-03 16:06:50 -0300

    aula 41

--- Assets/Sections/01-First Steps/Scripts/Introducer.cs
+++ Assets/Sections/01-First Steps/Scripts/Introducer.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class Introducer : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-    
     void Update()
     {
         transform.position += new Vector3(0.02f, 0f, 0f);
